fca48564c0a3116ba3fcfef2f3002e67f5f90868
Catherine Bacon <ckebacon@gmail.com>
2025-09-21 22:28:32 +0100

    Add language detection logic file

---
+++ language_detection.js
@@ -0,0 +1,8 @@
+if ('LanguageDetector' in self) {
+    // The Language Detector API is available.
+    // You can now write code to use it.
+    console.log("Language Detector API is available!");
+} else {
+    // The API is not available.
+    console.log("Language Detector API is not supported in this browser.");
+}
