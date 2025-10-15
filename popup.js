16797739e7520420c71eb4580467d3ba6a4f1712
Catherine Bacon <ckebacon@gmail.com>
2025-10-15 19:05:05 +0100

    Change processing popup height

--- popup.js
+++ popup.js
@@ -356,7 +356,7 @@ function loadSavedStatus(elements) {
         }
 
         if (status && status.progress > 0) {
-            document.body.style.height = '430px';
+            document.body.style.height = '300px';
 
             elements.statusBox.classList.remove('hidden-no-space');
             elements.statusText.textContent = status.message;
@@ -424,7 +424,7 @@ function loadSavedStatus(elements) {
 async function handleConfirmClick(elements) {
     console.log("[POPUP] 'Generate Subtitles' button clicked. Starting process.");
     
-    document.body.style.height = '430px';
+    document.body.style.height = '300px';
 
     isCancelledByPopup = false;
     elements.statusBox.classList.remove('hidden-no-space');
